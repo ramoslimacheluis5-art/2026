5ccdbe4702a77dee338d663b81eeea754be48ada
ramoslimacheluis5-art <ramoslimacheluis5@gmail.com>
2026-04-27 20:18:37 -0500

    Add files via upload

---
+++ OperacionesBasicas.java
@@ -0,0 +1,10 @@
+public class OperacionesBasicas {
+    public static void main(String[] args) {
+        int x = 12, y = 4;
+
+        System.out.println("Suma: " + (x + y));
+        System.out.println("Resta: " + (x - y));
+        System.out.println("Multi: " + (x * y));
+        System.out.println("Div: " + (x / y));
+    }
+}
