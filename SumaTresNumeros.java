cc5e71615e08dbbcfe97edc2c67698c7a17a2039
ramoslimacheluis5-art <ramoslimacheluis5@gmail.com>
2026-04-27 20:07:18 -0500

    Add files via upload

---
+++ SumaTresNumeros.java
@@ -0,0 +1,7 @@
+public class SumaTresNumeros {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 30;
+        int suma = a + b + c;
+        System.out.println("La suma es: " + suma);
+    }
+}
