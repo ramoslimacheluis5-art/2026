61265f29e048fbce2945719be1b2f8017d5fcb63
ramoslimacheluis5-art <ramoslimacheluis5@gmail.com>
2026-04-27 20:15:37 -0500

    Add files via upload

---
+++ ParOImpar.java
@@ -0,0 +1,10 @@
+public class ParOImpar{
+    public static void main(String[] args) {
+        int numero = 24;
+        if (numero % 2 == 0) {
+            System.out.println("24 es par");
+        } else {
+            System.out.println("24 es impar");
+        }
+    }
+}
